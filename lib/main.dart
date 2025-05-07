2a06b1a6486b48b1e6d46d6308fdb22a98454cba
mahmood.nasr <mahmood.nasr412@gmail.com>
2025-05-07 15:57:00 +0330

    user info transaction3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:hanigold_admin/src/config/const/app_color.dart';
@@ -11,7 +12,10 @@ import 'package:responsive_framework/responsive_framework.dart';
 
 void main() async{
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  EasyLoading.init();
+  configLoading();
 }
 
 class MyApp extends StatelessWidget {
@@ -20,23 +24,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //final isDesktop = ResponsiveBreakpoints.of(context).largerThan(TABLET);
     return GetMaterialApp(
       scrollBehavior: const MaterialScrollBehavior().copyWith(
-        dragDevices:{ PointerDeviceKind.touch},
+        dragDevices: {PointerDeviceKind.mouse},
       ),
-      builder: (context, child) => ResponsiveBreakpoints.builder(
-        child: child!,
-        breakpoints: [
-          const Breakpoint(start: 0, end: 500, name: MOBILE,),
-          const Breakpoint(start: 501, end: 700, name: TABLET),
-          const Breakpoint(start: 701, end: 1920, name: DESKTOP),
-          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-        ],
-      ),
-      theme: ThemeData(
 
-          appBarTheme: AppBarTheme(backgroundColor: AppColor.backGroundColor),
+      builder: (context, child) =>
+          ResponsiveBreakpoints.builder(
+            child: child!,
+            breakpoints: [
+              const Breakpoint(start: 0, end: 500, name: MOBILE,),
+              const Breakpoint(start: 501, end: 700, name: TABLET),
+              const Breakpoint(start: 701, end: 1920, name: DESKTOP),
+              const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+            ],
+          ),
+
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(backgroundColor: AppColor.backGroundColor),
         scaffoldBackgroundColor: AppColor.backGroundColor1,
         iconTheme: IconThemeData(color: AppColor.textColor),
       ),
@@ -60,4 +65,18 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+    ..loadingStyle = EasyLoadingStyle.dark
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.yellow
+    ..backgroundColor = Colors.green
+    ..indicatorColor = Colors.yellow
+    ..textColor = Colors.yellow
+    ..maskColor = Colors.blue.withOpacity(0.5)
+    ..userInteractions = true
+    ..dismissOnTap = false;
+}
