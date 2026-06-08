a421a203a61dc54cd7b3afcaa3161ec4c117f038
Peyman <peymantaghipour.fp@gmail.com>
2026-06-08 12:41:01 +0330

    change backgroundColor appDrawer

--- lib/src/widget/app_drawer.widget.dart
+++ lib/src/widget/app_drawer.widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hanigold_admin/src/config/const/app_color.dart';
 import 'package:hanigold_admin/src/widget/side_menu_fix.widget.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -8,20 +7,19 @@ class AppDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: 300,
-      height: Get.height*0.85,
-      color: AppColor.backGroundColor.withOpacity(0),
+      height: Get.height * 0.85,
       child: const Drawer(
-        shadowColor:
-            Colors.black26,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(20),
             bottomLeft: Radius.circular(20),
+          ),
         ),
-        ),
-        backgroundColor: AppColor.backGroundColor,
         child: SideMenuFix(),
       ),
     );
